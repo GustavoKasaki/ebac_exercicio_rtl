5748e42985dc7222a64b8aa7b66b71575b0909a1
GustavoKasaki <gmkasaki@gmail.com>
2025-01-24 12:37:27 -0300

    Criado teste para verificar a renderização de 02 comentários

---
+++ src/components/PostComments/__tests__/PostComments.test.tsx
@@ -0,0 +1,32 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import Post from "..";
+
+describe('Testes para os comentários', () => {
+    test('Deve renderizar 02 comentários', () => {
+        render(<Post />)
+
+        const input = screen.getByTestId('input-comment')
+        const botao = screen.getByTestId('botao-comment')
+
+        // primeiro comentario
+        fireEvent.change(input, {
+            target: {
+                value: 'Muito bom!'
+            }
+        })
+        fireEvent.click(botao)
+        expect(screen.getByText('Muito bom!')).toBeInTheDocument()
+
+        // segundo comentario
+        fireEvent.change(input, {
+            target: {
+                value: 'Top!'
+            }
+        })
+        fireEvent.click(botao)
+        expect(screen.getByText('Muito bom!')).toBeInTheDocument()
+
+        // verificação
+        expect(screen.getAllByTestId('comment')).toHaveLength(2)
+    })
+})
